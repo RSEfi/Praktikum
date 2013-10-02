dea22b2d74671b19c00a608641c9856b2aa78725
RSEfi <rsefi@web.de>
2013-10-02 14:01:09 +0200

    Main class created

---
+++ src/aufgabe1_1/Complex.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package aufgabe1_1;
+
+/**
+ * @author Daniel
+ * @version 2013-10-02
+ */
+public class Complex {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
