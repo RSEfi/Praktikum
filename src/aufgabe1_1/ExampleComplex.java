7a04a7f868a872a40d0763a72e5e562780385d1d
RSEfi <rsefi@web.de>
2013-10-05 19:20:18 +0200

    Requirement 11 created

---
+++ src/aufgabe1_1/ExampleComplex.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package aufgabe1_1;
+
+/**
+ * @author Daniel
+ *
+ */
+public class ExampleComplex {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Complex example = new Complex();
+	
+		System.out.println(example.toString());
+		System.out.println("Realteil: " + example.getReal());
+		System.out.println("Imaginärteil: " + example.getImag());
+
+	}
+
+}
